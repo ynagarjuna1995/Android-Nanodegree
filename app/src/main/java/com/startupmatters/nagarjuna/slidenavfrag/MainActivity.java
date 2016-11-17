fdb488dde2aee9fd449e28b639491d6d5d88ca1d
Nagarjuna <ynagarjuna2012@gmail.com>
2016-11-17 16:42:54 +0530

    Toolbar added

--- app/src/main/java/com/startupmatters/nagarjuna/slidenavfrag/MainActivity.java
+++ app/src/main/java/com/startupmatters/nagarjuna/slidenavfrag/MainActivity.java
@@ -1,13 +1,22 @@
 package com.startupmatters.nagarjuna.slidenavfrag;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    private Toolbar mToolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main);
+
+        mToolbar = (Toolbar) findViewById(R.id.toolbar);
+
+        setSupportActionBar(mToolbar);
     }
 }
